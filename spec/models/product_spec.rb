ae737df1023d9af699f86aaf9de5a16dc49cac6b
djtwdix <djtwd.ix@icloud.com>
2021-04-05 14:57:19 -0300

    Test for relevant error message

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -16,22 +16,24 @@ RSpec.describe Product, type: :model do
                              category: @category,
                              quantity: 10)
       expect(@product).to_not be_valid
-    end 
-
-    it "is not valid with no price" do
-      @category = Category.new(name: "Furniture")
-      @product = Product.new(name: "Product", price: 1000, 
-                             category: nil,
-                             quantity: 10)
-      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end 
 
     it "is not valid with no category" do
       @category = Category.new(name: "Furniture")
       @product = Product.new(name: "Product", price: nil, 
-                             category: @category,
+                             category: nil,
                              quantity: 10)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Price can't be blank")
+    end 
+
+    it "is not valid with no category" do
+      @category = Category.new(name: "Furniture")
+      @product = Product.new(name: "Product", price: 1000, category: nil, quantity: 10)
+      expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+     
     end 
 
     it "is not valid with no quantity" do
@@ -40,6 +42,7 @@ RSpec.describe Product, type: :model do
                              category: @category,
                              quantity: nil)
       expect(@product).to_not be_valid
+      expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end 
   end
 end
