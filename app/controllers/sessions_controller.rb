85667daa3e92f42208e7c8e8dc97bd39be84e2b4
djtwdix <djtwd.ix@icloud.com>
2021-04-05 17:53:43 -0300

    Add auth with credentials method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    puts user.inspect
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+    if user = User.authenticate_with_credentials(params[:email], params[:password])
       session[:user_id] = user.id
       redirect_to '/'
+      # success logic, log them in
     else
-    # If user's login doesn't work, send them back to the login form.
       redirect_to '/login'
+      # failure, render login form
     end
   end
 
